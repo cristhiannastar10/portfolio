44f4d97b843e20e1f6eec1949be9bf8ea0cfca2c
cristhiannastar10 <nastarjunior10@gmail.com>
2024-04-07 18:56:11 -0500

    primer compromiso

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className= "font-roboto felz justify-center items-center">
+     <h1 className="text-3xl font-bold text-red-700">hello world </h1>
+    </main>
+
+  );
+}
+
+export default App;
